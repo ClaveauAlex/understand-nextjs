dc4e2b0cbe63759b00be6359733507b580f7670b
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-01 18:42:21 +0100

    Récupération des données avec getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
 import React from "react";
-import {Layout} from "../components/layout";
+import { Layout } from "../components/layout";
+import Axios from "axios";
 
-const Home = () => {
+const Home = ({ data }) => {
+  const styles = {
+    padding: 10,
+    margin: 10,
+    borderBottom: "1px solid #ddd"
+  }
   return (
     <Layout>
-      Bonjour
+      {data.map((region) => (
+        <div style={styles}>
+          <h1>{region.nom}</h1>
+          <p>{region.code}</p>
+        </div>
+      ))}
     </Layout>
-  )
+  );
+};
+
+export async function getServerSideProps(context) {
+  const url = "https://geo.api.gouv.fr";
+  const { data } = await Axios.get(url + "/regions");
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
 
 export default Home;
