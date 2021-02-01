c769733b7fddc4b7aa9fc4e2ea8a16fc05f924b5
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-01 15:53:57 +0100

    Refactoring de la page blog

--- pages/blog.js
+++ pages/blog.js
@@ -2,31 +2,26 @@ import React from "react";
 import {Layout} from "../components/layout";
 import Link from "next/link";
 
+const PostLink = ({titre}) => {
+    return (
+        <li>
+            <Link href={"/blog/[titre]"} as={`/blog/${titre}`}>
+                <a>{titre}</a>
+            </Link>
+        </li>
+    )
+}
+
 const Blog = () => {
     return (
         <Layout>
             <h1>Blog</h1>
             <ul>
-                <li>
-                    <Link href={"/blog/[titre]"} as={"/blog/react"}>
-                        <a>React</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/blog/[titre]"} as={"/blog/angular"}>
-                        <a>Angular</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/blog/[titre]"} as={"/blog/vue"}>
-                        <a>Vue</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href={"/blog/[titre]"} as={"/blog/svelte"}>
-                        <a>Svelte</a>
-                    </Link>
-                </li>
+                <PostLink titre={"react"}/>
+                <PostLink titre={"angular"}/>
+                <PostLink titre={"vue"}/>
+                <PostLink titre={"svelte"}/>
+                <PostLink titre={"apprendre Nextjs"}/>
             </ul>
         </Layout>
     )
