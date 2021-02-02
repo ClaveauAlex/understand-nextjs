09d45267d989516c4221c3a5b88b0b806e726d72
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-02 11:28:22 +0100

    Styler les composants avec styled-jsx

--- components/header.js
+++ components/header.js
@@ -1,44 +1,79 @@
 import React from "react";
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 export const Header = () => {
-    const router = useRouter();
-    const styles = {
-        header: {
-            margin: 20,
-            padding: 20,
-            border: "1px solid #ddd"
-        },
-        link: {
-            margin: 15
-        },
-        active: {
-            margin: 15,
-            color: "blue"
-        }
-    }
+  const router = useRouter();
+  const styles = {
+    header: {
+      margin: 20,
+      padding: 20,
+      border: "1px solid #ddd",
+    },
+    link: {
+      margin: 15,
+    },
+    active: {
+      margin: 15,
+      color: "blue",
+    },
+  };
 
-    return (
-        <div style={styles.header}>
-            <Link href={"/"} passHref>
-                <span style={router.pathname === "/" ? styles.active : styles.link}>Home</span>
-            </Link>
-            <Link href={"/blog"} passHref>
-                <span style={router.pathname === "/blog" ? styles.active : styles.link}>Blog</span>
-            </Link>
-            <Link href={"/profile"} passHref>
-                <span style={router.pathname === "/profile" ? styles.active : styles.link}>Profile</span>
-            </Link>
-            <Link href={"/blog/items"} passHref>
-                <span style={router.pathname === "/blog/items" ? styles.active : styles.link}>Items</span>
-            </Link>
-            <Link href={"/blog/categories"} passHref>
-                <span style={router.pathname === "/blog/categories" ? styles.active : styles.link}>Categories</span>
-            </Link>
-            <Link href={"/departements"} passHref>
-                <span style={router.pathname === "/departements" ? styles.active : styles.link}>Departements</span>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <Link href={"/"} passHref>
+        <span style={router.pathname === "/" ? styles.active : styles.link}>
+          Home
+        </span>
+      </Link>
+      <Link href={"/blog"} passHref>
+        <span style={router.pathname === "/blog" ? styles.active : styles.link}>
+          Blog
+        </span>
+      </Link>
+      <Link href={"/profile"} passHref>
+        <span
+          style={router.pathname === "/profile" ? styles.active : styles.link}
+        >
+          Profile
+        </span>
+      </Link>
+      <Link href={"/blog/items"} passHref>
+        <span
+          style={
+            router.pathname === "/blog/items" ? styles.active : styles.link
+          }
+        >
+          Items
+        </span>
+      </Link>
+      <Link href={"/blog/categories"} passHref>
+        <span
+          style={
+            router.pathname === "/blog/categories" ? styles.active : styles.link
+          }
+        >
+          Categories
+        </span>
+      </Link>
+      <Link href={"/departements"} passHref>
+        <span
+          style={
+            router.pathname === "/departements" ? styles.active : styles.link
+          }
+        >
+          Departements
+        </span>
+      </Link>
+      <style jsx>
+        {`
+          .header {
+            margin: 20px;
+            padding: 20px;
+            border: 1px solid #ddd;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
